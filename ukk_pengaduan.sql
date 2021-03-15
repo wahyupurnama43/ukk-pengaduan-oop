75df2c84f4fcc44ce9e8f6954a94604071811fee
wahyupurnama434@gmail.com <wahyupurnama434@gmail.com>
2021-03-15 16:22:53 +0800

    add database

--- ukk_pengaduan.sql
+++ ukk_pengaduan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 13 Mar 2021 pada 20.29
+-- Waktu pembuatan: 15 Mar 2021 pada 16.21
 -- Versi server: 5.7.33-0ubuntu0.18.04.1
 -- Versi PHP: 7.4.14
 
@@ -40,7 +40,7 @@ CREATE TABLE `masyarakat` (
 --
 
 INSERT INTO `masyarakat` (`nik`, `nama`, `username`, `password`, `telp`) VALUES
-('27255', 'wahyu purnama', 'wahyu', '$2y$10$zpjJhhtveKdGuPBjLai8AeaHU8BnahPSdwPqqN3xzHcKcVUbnYNFO', '098721312');
+('28901', 'wahyu', 'wahyu', '$2y$10$bKlRv8U5H/9N4W2MLmSr3O1.edgY4r2kB9C9ctks2diYkCyeYiMXe', '098732123');
 
 -- --------------------------------------------------------
 
@@ -50,12 +50,19 @@ INSERT INTO `masyarakat` (`nik`, `nama`, `username`, `password`, `telp`) VALUES
 
 CREATE TABLE `pengaduan` (
   `id_pengaduan` int(11) NOT NULL,
-  `tgl_pengaduan` date NOT NULL,
+  `tgl_pengaduan` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   `nik` char(16) NOT NULL,
   `isi_laporan` text NOT NULL,
   `foto` varchar(255) NOT NULL,
   `status` enum('0','proses','selesai','') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pengaduan`
+--
+
+INSERT INTO `pengaduan` (`id_pengaduan`, `tgl_pengaduan`, `nik`, `isi_laporan`, `foto`, `status`) VALUES
+(11, '2021-03-15 16:15:36', '28901', 'siap', '2113182500604dff411670a.png', 'selesai');
 
 -- --------------------------------------------------------
 
@@ -78,21 +85,28 @@ CREATE TABLE `petugas` (
 
 INSERT INTO `petugas` (`id_petugas`, `nama_petugas`, `username`, `password`, `telp`, `level`) VALUES
 (1, 'admin', 'admin', '$2y$10$mC9Va4awVYMf3NcVvzLw1.AwWwc4qz/LsanvuAHOcAxhiEpeaAcGy', '09', 'admin'),
-(6, 'yudha', 'yudha', '$2y$10$MtrBhTPvTLwxWRf4i6UThuLl3aONJhrZX70UKK0NOQs.8Tv07bXge', '087621231', 'admin');
+(6, 'yudha3', 'yudha', '$2y$10$MtrBhTPvTLwxWRf4i6UThuLl3aONJhrZX70UKK0NOQs.8Tv07bXge', '087621231', 'petugas');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tannggapan`
+-- Struktur dari tabel `tanggapan`
 --
 
-CREATE TABLE `tannggapan` (
+CREATE TABLE `tanggapan` (
   `id_tanggapan` int(11) NOT NULL,
   `id_pengaduan` int(11) NOT NULL,
-  `tgl_tanggapan` date NOT NULL,
+  `tgl_tanggapan` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `tanggapan` text NOT NULL,
   `id_petugas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tanggapan`
+--
+
+INSERT INTO `tanggapan` (`id_tanggapan`, `id_pengaduan`, `tgl_tanggapan`, `tanggapan`, `id_petugas`) VALUES
+(1, 11, '2021-03-15 16:15:36', 'baik', 1);
 
 --
 -- Indexes for dumped tables
@@ -118,9 +132,9 @@ ALTER TABLE `petugas`
   ADD PRIMARY KEY (`id_petugas`);
 
 --
--- Indeks untuk tabel `tannggapan`
+-- Indeks untuk tabel `tanggapan`
 --
-ALTER TABLE `tannggapan`
+ALTER TABLE `tanggapan`
   ADD PRIMARY KEY (`id_tanggapan`),
   ADD KEY `id_pengaduan` (`id_pengaduan`),
   ADD KEY `id_petugas` (`id_petugas`);
@@ -133,7 +147,7 @@ ALTER TABLE `tannggapan`
 -- AUTO_INCREMENT untuk tabel `pengaduan`
 --
 ALTER TABLE `pengaduan`
-  MODIFY `id_pengaduan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pengaduan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT untuk tabel `petugas`
@@ -142,21 +156,21 @@ ALTER TABLE `petugas`
   MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT untuk tabel `tannggapan`
+-- AUTO_INCREMENT untuk tabel `tanggapan`
 --
-ALTER TABLE `tannggapan`
-  MODIFY `id_tanggapan` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `tanggapan`
+  MODIFY `id_tanggapan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Ketidakleluasaan untuk tabel `tannggapan`
+-- Ketidakleluasaan untuk tabel `tanggapan`
 --
-ALTER TABLE `tannggapan`
-  ADD CONSTRAINT `tannggapan_ibfk_1` FOREIGN KEY (`id_pengaduan`) REFERENCES `pengaduan` (`id_pengaduan`),
-  ADD CONSTRAINT `tannggapan_ibfk_2` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id_petugas`);
+ALTER TABLE `tanggapan`
+  ADD CONSTRAINT `tanggapan_ibfk_2` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id_petugas`),
+  ADD CONSTRAINT `tanggapan_ibfk_3` FOREIGN KEY (`id_pengaduan`) REFERENCES `pengaduan` (`id_pengaduan`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
